52864b206d88ff925428174ec02ce4579495d13a
Ae-Mc <ae_mc@mail.ru>
2020-10-31 16:22:47 +0300

    Write converters from pdf and image to text

---
+++ ToText.py
@@ -0,0 +1,27 @@
+import fitz
+from os import system, remove
+from tempfile import TemporaryFile
+
+
+def imageToText(imageFilename: str) -> str:
+    with TemporaryFile(mode='r', encoding="utf-8") as tempfile:
+        system(f'tesseract -l "rus+eng" "{imageFilename}" "{tempfile.name}"')
+        with open(tempfile.name + '.txt', encoding='utf-8') as f:
+            return f.read()
+
+
+def pdfToText(pdfFilename: str) -> str:
+    doc: fitz.Document = fitz.open(pdfFilename)
+    text = ""
+    tempfileName = TemporaryFile().name
+    for i in range(len(doc)):
+        for j, img in enumerate(doc.getPageImageList(i)):
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            if pix.n > 4:
+                pix = fitz.Pixmap(fitz.csRGB, pix)
+            pix.writePNG(f"{tempfileName}")
+            text += imageToText(f'{tempfileName}') + '\n'
+    remove(tempfileName)
+    doc.close()
+    return text
